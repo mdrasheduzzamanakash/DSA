ecc8d5436d6420a281f08a1f40dd491c9b6c41da
Irfanul Hoque <irhq13@gmail.com>
2022-06-17 01:23:48 +0600

    Selective exploration LC718

--- Leetcode/implementations/718-max-length-of-repeated-subarray.cpp
+++ Leetcode/implementations/718-max-length-of-repeated-subarray.cpp
@@ -2,13 +2,25 @@ class Solution {
   public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        int length = 0;
+        int m = nums2.size();
 
-        for (int i = 0; i < n - length; i++)
-            for (int j = 0; j < n - length; j++)
-                for (int k = 0; i + k < n and j + k < n and nums1[i + k] == nums2[j + k]; k++)
+        unordered_map<int, vector<int>> beginsWith;
+        for (int i = 0; i < m; i++)
+            beginsWith[nums2[i]].push_back(i);
+
+        int length = 0;
+        for (int i = 0; i < n - length; i++) {
+            for (int j : beginsWith[nums1[i]]) {
+                if (m - j <= length)
+                    continue;
+                for (int k = 0; i + k < n and j + k < m and nums1[i + k] == nums2[j + k]; k++)
                     length = max(k + 1, length);
+            }
+        }
 
         return length;
     }
 };
+
+// Insight: most of the time nums1[i] != nums2[j]
+// Only explore for those j where nums1[i] == nums2[j]
