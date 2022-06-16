00b6a6c7cdd8e751970526f2cae81d03f30bb360
Rashed <mdrasheduzzamanakash@gmail.com>
2022-06-16 23:39:10 +0600

    Added number theory problems

---
+++ Leetcode/number-theory/914-x-of-a-kind-in-a-Deck-of-Cards.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        map<int,int> cnt;
+        for(int i = 0;i < deck.size();i++) {
+            cnt[deck[i]]++;
+        }
+        int gcd = 0;
+        for(auto I : cnt) {
+            gcd = __gcd(gcd, I.second);
+        }
+        return gcd >= 2;
+    }
+};
